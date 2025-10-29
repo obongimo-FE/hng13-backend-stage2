da6af0f31c9f5925bd6cb28bd7202bf979306e11
obongimo-FE <obgmmo@gmail.com>
2025-10-29 05:18:30 +0100

    modification for railway connection

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
+import 'dotenv/config';
 import mysql from 'mysql2/promise';
-import env from "dotenv";
 
-export const db = await mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USERNAME || 'root',       // your MySQL username
-  password: process.env.DB_PASSWORD || '',  // your MySQL password
-  database: process.env.DB_DATABASE || 'countries_db', // your database name
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
-});
+export const db = await mysql.createConnection(process.env.DATABASE_URL);
+
+
+
+
+
+
+
+// {
+
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USERNAME || 'root',       // your MySQL username
+//   password: process.env.DB_PASSWORD || '',  // your MySQL password
+//   database: process.env.DB_DATABASE || 'countries_db', // your database name
+//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+// }
